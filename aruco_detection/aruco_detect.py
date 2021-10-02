b5a7b67916a6b2698cc15ebf92902c6686b40388
Oscar Alvarez <mathoscaralvarez@gmail.com>
2021-10-02 01:28:26 -0500

    aruco update1

--- aruco_detection/aruco_detect.py
+++ aruco_detection/aruco_detect.py
@@ -1,16 +1,22 @@
 import cv2 as cv
 import numpy as np
 
-# Set up CV2 Aruco 
-ar_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_100)
-ar_params = cv.aruco.DetectorParameters_create()
+class aruco_detect:
 
-frame = cv.imread('boxesWithArucoTags.png')
+    def __init__(self):
+        # Set up CV2 Aruco 
+        self.ar_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_100)
+        self.ar_params = cv.aruco.DetectorParameters_create()
+        self.ar_counts = {}
+        self.total = 0
 
-ar_corners, ar_ids, rejects = cv.aruco.detectMarkers(frame, ar_dict, parameters=ar_params)
 
-print(ar_ids)
+    def get_tags(self, frame) -> dict:
+        ar_corners, ar_ids, rejects = cv.aruco.detectMarkers(frame, self.ar_dict, parameters=self.ar_params)
+        
+        # Counts the number of each ar tag
+        ar_set = set(ar_ids)
+        self.ar_counts = dict((x, ar_ids.count(x) for x in ar_set))
+        
+        self.total = len(ar_ids)
 
-total = np.sum(ar_ids)
-
-print("Total Boxes: ", total)
