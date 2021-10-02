58c9b32ef71c49cb1cce77829fb480df6d606ffe
Oscar Alvarez <mathoscaralvarez@gmail.com>
2021-10-02 11:59:53 -0500

    new version same functionality aruco update
    Signed-off-by: Oscar Alvarez <mathoscaralvarez@gmail.com>

--- aruco_detection/aruco_detect.py
+++ aruco_detection/aruco_detect.py
@@ -9,25 +9,40 @@ class aruco_detect:
         self.ar_params = cv.aruco.DetectorParameters_create()
         self.ar_counts = {'0': 0, '1': 0, '2': 0}
         self.total = 0
+        self.gate_no = np.array([10, 20, 30])
+        for g in self.gate_no:
+            self.gates = {g, False}
+        
 
-
-    def get_tags(self, frame):
+    def draw_tags(self, frame):
         # Detects AR tags
         ar_corn, ar_ids, rejects = cv.aruco.detectMarkers(frame, self.ar_dict, parameters=self.ar_params)
-        self.ar_counts = {'0': 0, '1': 0, '2': 0}
-        self.total = 0
-        if np.all(ar_ids != None):
-            # Counts the number of each ar tag
-            unique, counts = np.unique(ar_ids, return_counts=True)
-            self.ar_counts = dict(zip(unique.astype(str), counts))
-
-            # Total number of tags/objects
-            self.total = len(ar_ids)
+        
+        if np.all(ar_ids == None):
+            return(frame)
+        else:
+            # Draw border and label ar tags
+            ar_frame = cv.aruco.drawDetectedMarkers(frame, ar_corn, ar_ids)
+            return(ar_frame)
             
+    
+    def draw_tags_count(self, frame):
+        # Detects AR tags
+        ar_corn, ar_ids, rejects = cv.aruco.detectMarkers(frame, self.ar_dict, parameters=self.ar_params)
+        
+        if np.all(ar_ids == None):
+            return(frame)
+        else:
+            self.count_tags(ar_ids)
             # Draw Outline around AR tags
             ar_frame = cv.aruco.drawDetectedMarkers(frame, ar_corn, ar_ids)
-            
             return(ar_frame)
-        else:
-            return(frame)
-            
+
+
+    def count_tags(self, ar_ids):
+        # Counts the number of each ar tag
+        unique, counts = np.unique(ar_ids, return_counts=True)
+        self.ar_counts = dict(zip(unique.astype(str), counts))
+
+        # Total number of tags/objects
+        self.total = len(ar_ids)
